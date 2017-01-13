bc82948d8438002a0fcb678b9e92c6e23da7af5e
gallanonimm <kacperek.bilko@gmail.com>
2017-01-14 00:35:26 +0100

    final version

--- guii.m
+++ guii.m
@@ -22,7 +22,7 @@ function varargout = guii(varargin)
 
 % Edit the above text to modify the response to help guii
 
-% Last Modified by GUIDE v2.5 12-Jan-2017 19:00:06
+% Last Modified by GUIDE v2.5 13-Jan-2017 23:23:07
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -58,9 +58,7 @@ guidata(hObject, handles);
 
 % This sets up the initial plot - only do when we are invisible
 % so window can get raised using guii.
-if strcmp(get(hObject,'Visible'),'off')
-    plot(rand(5));
-end
+
 
 % UIWAIT makes guii wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
@@ -76,90 +74,6 @@ function varargout = guii_OutputFcn(hObject, eventdata, handles)
 % Get default command line output from handles structure
 varargout{1} = handles.output;
 
-% --- Executes on button press in pushbutton1.
-function pushbutton1_Callback(hObject, eventdata, handles)
-% hObject    handle to pushbutton1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-axes(handles.axes1);
-cla;
-popup_sel_index = get(handles.popupmenu1, 'Value');
-switch popup_sel_index
-    case 1
-        plot(rand(5));
-    case 2
-        plot(sin(1:0.01:25.99));
-    case 3
-        bar(1:.5:10);
-    case 4
-        plot(membrane);
-    case 5
-        surf(peaks);
-end
-
-
-% --------------------------------------------------------------------
-function FileMenu_Callback(hObject, eventdata, handles)
-% hObject    handle to FileMenu (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
-% --------------------------------------------------------------------
-function OpenMenuItem_Callback(hObject, eventdata, handles)
-% hObject    handle to OpenMenuItem (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-file = uigetfile('*.fig');
-if ~isequal(file, 0)
-    open(file);
-end
-
-% --------------------------------------------------------------------
-function PrintMenuItem_Callback(hObject, eventdata, handles)
-% hObject    handle to PrintMenuItem (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-printdlg(handles.figure1)
-
-% --------------------------------------------------------------------
-function CloseMenuItem_Callback(hObject, eventdata, handles)
-% hObject    handle to CloseMenuItem (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-selection = questdlg(['Close ' get(handles.figure1,'Name') '?'],...
-                     ['Close ' get(handles.figure1,'Name') '...'],...
-                     'Yes','No','Yes');
-if strcmp(selection,'No')
-    return;
-end
-
-delete(handles.figure1)
-
-
-% --- Executes on selection change in popupmenu1.
-function popupmenu1_Callback(hObject, eventdata, handles)
-% hObject    handle to popupmenu1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: contents = get(hObject,'String') returns popupmenu1 contents as cell array
-%        contents{get(hObject,'Value')} returns selected item from popupmenu1
-
-
-% --- Executes during object creation, after setting all properties.
-function popupmenu1_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to popupmenu1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: popupmenu controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-     set(hObject,'BackgroundColor','white');
-end
-
-set(hObject, 'String', {'plot(rand(5))', 'plot(sin(1:0.01:25))', 'bar(1:.5:10)', 'plot(membrane)', 'surf(peaks)'});
 
 
 % --- Executes on button press in start.
@@ -177,19 +91,40 @@ function start_Callback(hObject, eventdata, handles)
     q = scalarsTableData(2);
     tmax = scalarsTableData(3);
     dt = scalarsTableData(4);
-    dr = scalarsTableData(5);
     E = str3D2func(E_strTab);
     B = str3D2func(B_strTab);
+    resonableTimeParameter = 1e4;
+    if(tmax/dt> resonableTimeParameter)
+        msgbox('Trwaj¹ obliczenia... Proszê czekaæ!');
+    end
+    
+    f = @(t,v)[ v(4); v(5); v(6);
+         q *(E{1}(v(1),v(2),v(3)) + v(5) * B{3}(v(1),v(2),v(3)) - v(6) * B{2}(v(1),v(2),v(3)));
+         q *(E{2}(v(1),v(2),v(3)) + v(6) * B{1}(v(1),v(2),v(3)) - v(4) * B{3}(v(1),v(2),v(3)));
+         q *(E{3}(v(1),v(2),v(3)) + v(4) * B{2}(v(1),v(2),v(3)) - v(5) * B{1}(v(1),v(2),v(3)));];
 
-    %cos w tym stylu chyba, tylko nie mam sil juz tego testowac
-    f = @(t,v)[ v(4); v(5); v(6); 
-        q/m * (E{1}(v(1),v(2),v(3)) + v(5) * B{3}(v(1),v(2),v(3)) - v(6) * B{2}(v(1),v(2),v(3)));
-        q/m * (E{2}(v(1),v(2),v(3)) + v(6) * B{1}(v(1),v(2),v(3)) - v(4) * B{3}(v(1),v(2),v(3)));
-        q/m * (E{3}(v(1),v(2),v(3)) + v(4) * B{2}(v(1),v(2),v(3)) - v(5) * B{1}(v(1),v(2),v(3)));];
-    [tt, vv] = ode45(f,[0 tmax],[r0(1) r0(2) r0(3) v0(1) v0(2) v0(3)]);
-    %syms t
-    %ezplot3(vv(1), vv(2), vv(3),[0 tmax])
+    [tt, vv] = ode45(f,0:dt:tmax,[r0(1) r0(2) r0(3) v0(1) v0(2) v0(3)]);
+    
+    vv = vv/m;
+     
+    ploter(handles.fig5, @plot3, {vv(:,1),vv(:,2),vv(:,3)}, 'trajektoria', 'x [m]', 'y [m]', 'z [m]');
+    ploter(handles.fig1, @plot, {tt,sqrt(vv(:,4).^2+vv(:,5).^2+vv(:,6).^2)}, 'v(t)', 't [s]', 'v [m/s]','');
+    ploter(handles.fig2, @plot, {tt,vv(:,1)}, 'x(t)', 't [s]', 'x [m]','');
+    ploter(handles.fig3, @plot, {tt,vv(:,2)}, 'y(t)', 't [s]', 'y [m]','');
+    ploter(handles.fig4, @plot, {tt,vv(:,3)}, 'z(t)', 't [s]', 'z [m]','');
+    guidata(hObject, handles);
 
+    
+  function ploter(plotHandler,funHandler,data,titleStr, xlabelStr, ylabelStr, zlabelStr)
+    axes(plotHandler);
+    funHandler(data{:});
+    title(titleStr);
+    grid on;
+    xlabel(xlabelStr);
+    ylabel(ylabelStr);
+    zlabel(zlabelStr);
+   
+ 
 function funcCellArray = str3D2func(strTab)
     funcBeginning = '@(x,y,z)';
     x = str2func(strcat(funcBeginning,strTab(1,:)));
@@ -197,8 +132,5 @@ function funcCellArray = str3D2func(strTab)
     z = str2func(strcat(funcBeginning,strTab(3,:)));
     funcCellArray = {x, y, z};
 
-% --- Executes on button press in stop.
-function stop_Callback(hObject, eventdata, handles)
-% hObject    handle to stop (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
+
+        
